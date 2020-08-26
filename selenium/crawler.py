7c53c1944cce886afe1a8650921c78cb45cafd7d
Zach Wingrave <z.wingrave@gmail.com>
2020-08-26 22:16:22 +1000

    Update URL

--- selenium/crawler.py
+++ selenium/crawler.py
@@ -2,5 +2,16 @@ from selenium import webdriver
 
 PATH = r"C:\Program Files (x86)\Webdrivers\chromedriver.exe"
 
+label = input("Enter tracking number: ")
+
 app = webdriver.Chrome(PATH)
-app.get("http://seleniumhq.org/")
+app.get("https://www.aramex.com.au/tools/track/")
+
+field = app.find_element_by_id("label_number")
+""" Enter label into text area """
+
+submit = app.find_element_by_id("trace_button")
+""" Click on track button """
+
+
+app.quit()
