c12d9beb4b13b0653c10acc519ea96fa580f4298
Zach Wingrave <z.wingrave@gmail.com>
2020-08-27 12:48:28 +1000

    Added working Postman query

--- fastway/fastway_client.py
+++ fastway/fastway_client.py
@@ -1,41 +1,24 @@
-import json, pprint, requests
-from os import system, name
-
-def clear_screen():
-    # for windows
-    if name == 'nt':
-        _ = system('cls')
-    # for mac and linux(here, os.name is 'posix')
-    else: 
-        _ = system('clear')
-
-def print_request(r):
-    print("HTTP Code", r.status_code)
-    if r.text != "" and r.text != None:
-        data = json.loads(r.text)
-        print(json.dumps(data, indent=4, sort_keys=True))
-    else:
-        print("No response.")
+from fastway_utils import requests, clear_screen, print_request
 
 def get_token():
     url = "https://identity.fastway.org/connect/token"
 
-    grant_type = "{grant_type}"
-    client_id = "{client_id}"
-    client_secret = "{client_secret}"
-    scope = "{scope}"
+    headers = {
+        "grant_type" : "{grant_type}",
+        "client_id" : "{client_id}",
+        "client_secret" : "{client_secret}",
+        "scope" : "{scope}"
+    }
 
-    auth = "?grant_type="+grant_type+"&client_id="+client_id+\
-        "&client_secret="+client_secret+"&scope="+scope
-    
-    r = requests.post(url + auth)
-    return r
+    return requests.post(url, headers=headers)
 
 def track_item(token, label):
     url = "https://api.myfastway.com.au/api/track/label/"
+    
+    with open("fastway_token",'r') as auth:
+        headers = { "Authorization" : auth.readlines()[0] }
 
-    r = requests.post(url + label)
-    return r
+    return requests.get(url + label, headers=headers)
 
 def main():
     clear_screen()
