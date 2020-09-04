1008e2fcb1899dfd7a3db3e80f89ecac5e401b45
Zach Wingrave <z.wingrave@gmail.com>
2020-09-04 12:26:04 +1000

    Test path with os.path and os.getcwd

--- fastway/fastway_client.py
+++ fastway/fastway_client.py
@@ -1,6 +1,10 @@
-from os import system, name
-from time import time
+from os import system, name, path, getcwd
+from datetime import datetime, timedelta
 import json, requests
+
+file = "fastway_auth.json"
+with open(file, "r") as file:
+    authorization = json.load(file)
 
 def clear_screen():
     if name == 'nt':
@@ -8,47 +12,66 @@ def clear_screen():
     else: 
         system('clear')
 
-def print_json(j):
-    print(json.dumps(j, indent=4, sort_keys=True))
-
-def print_request(r):
-    print("HTTP Code", r.status_code)
-    if r.text != "" and r.text != None:
-        print_json(json.loads(r.text))
-    else:
-        print("No response.")
-
 def get_token(authorization):
     url = "https://identity.fastway.org/connect/token"
-    return requests.post(url, data=authorization).text
+    response = requests.post(url, data=authorization)
 
-def get_token_header(j):
-    return { "Authorization": j["token_type"] + " " + j["access_token"] }
+    data = json.loads(response.text)
+    expiry = (datetime.now() + timedelta(hours=1)).isoformat()
+    data["expiry_date"] = expiry
+
+    print(json.dumps(data, indent=4, sort_keys=True))
+    with open("fastway_token.json", "w") as file:
+        json.dump(data, file, indent=4, sort_keys=True)
+    return response.status_code
+
+def get_token_expiry(token):
+    pass
+
+def get_token_header(token):
+    return { "Authorization": token["token_type"] + " " + token["access_token"] }
 
 def track_item(token_header, label):
     url = "https://api.myfastway.com.au/api/track/label/"
     return requests.get(url + label, headers=token_header)
 
-def main():
-    file = "fastway_auth.json"
-    with open(file, "r") as file:
-        authorization = json.load(file)
-
+def track_one_item():
     clear_screen()
     
     token = json.loads(get_token(authorization))
     token_header = get_token_header(token)
-    
-    label = input("Enter tracking number: ")
-    start = time()
-    result = track_item(token_header, label)
-    print_request(result)
 
-    end = time()
-    print("Took", end-start, "seconds to fetch.")
+    label = input("Enter tracking number: ")
+    response = track_item(token_header, label)
+
+    print("HTTP Code", response.status_code)
+    if response.text != "" and response.text != None:
+        print(response.json())
+        #print(json.dumps(response.text, indent=4, sort_keys=True))
+        #print_json(json.loads(response.text))
+    else:
+        print("No response.")
 
     input("Press [ENTER] to exit.")
-    
     clear_screen()
 
-main()
+def main():
+    clear_screen()
+    
+    print("Generating token into fastway_token.json...")
+
+    response = get_token(authorization)
+
+    if 200 <= response <= 229:
+        print("Succeeded with HTTP", response)
+    else:
+        print("Failed with HTTP", response)
+
+    input("Press [ENTER] to exit.")
+    clear_screen()
+
+def get_path():
+    print(getcwd())
+    print(path.abspath(getcwd()))
+
+get_path()
