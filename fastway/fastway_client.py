e290b3c9a96da6260dcf5b6270ece20866bd0024
Zach Wingrave <z.wingrave@gmail.com>
2020-09-04 17:28:16 +1000

    Refactor main functions

--- fastway/fastway_client.py
+++ fastway/fastway_client.py
@@ -1,52 +1,67 @@
 from datetime import datetime, timedelta
-from json import dump, load, loads
+from json import dump, dumps, load, loads
 from os import system, name, path
 from requests import get, post
 
-ROOT_DIR = path.dirname(path.abspath(__file__))
-AUTH_FILE = "/".join((ROOT_DIR, "fastway_auth.json"))
-TOKEN_FILE = "/".join((ROOT_DIR, "fastway_token.json"))
+if name == "nt":
+    SEP = "\\"
+    CLEAR = "cls"
+else:
+    SEP = "/"
+    CLEAR = "clear"
 
-def clear_screen():
-    if name == 'nt':
-        _ = system('cls')
-    else: 
-        _ = system('clear')
+ROOT_DIR = path.dirname(path.abspath(__file__))
+AUTH_FILE = SEP.join((ROOT_DIR, "fastway_auth.json"))
+TOKEN_FILE = SEP.join((ROOT_DIR, "fastway_token.json"))
 
 def get_path(file):
-    return "/".join((ROOT_DIR, file))
+    return SEP.join((ROOT_DIR, file))
 
 def get_authorization(file=AUTH_FILE):
-    with open(file, "r") as file:
-        return load(file)
+    try:
+        with open(file, "r") as file:
+            return load(file)
+    except FileNotFoundError as exception:
+        print(exception)
+        return False
 
 def get_token(file=TOKEN_FILE):
-    with open(file, "r") as file:
-        return load(file)
+    try:
+        with open(file, "r") as file:
+            token = load(file)
+            if datetime.now().isoformat() < token["token_expiry"]:
+                return token
+            else:
+                return renew_token(file)
+    except FileNotFoundError:
+        return renew_token(file)
 
-def renew_token():
+def renew_token(file=TOKEN_FILE):
+    print("Generating new token...")
     url = "https://identity.fastway.org/connect/token"
     response = post(url, data=get_authorization())
     
-    if 200 <= response <= 229:
-        print("Succeeded with HTTP", response)
-    else:
-        print("Failed with HTTP", response)
-    
-    data = loads(response.text)
+    token = loads(response.text)
     expiry = datetime.now() + timedelta(hours=1)
-    data["token_expiry"] = expiry.isoformat()
+    token["token_expiry"] = expiry.isoformat()
 
-    with open(TOKEN_FILE, "w") as file:
-        dump(data, file, indent=4, sort_keys=True)
-    return response.status_code
+    with open(file, "w") as file:
+        dump(token, file, indent=4, sort_keys=True)
+    
+    return token
 
 def get_expiry(token=get_token()):
-    return token["token_expiry"]
+    if token:
+        return token["token_expiry"]
+    else:
+        return False
 
 def get_header(token=get_token()):
-    credentials = (token["token_type"], token["access_token"])
-    return { "Authorization": " ".join(credentials) }
+    if token:
+        credentials = (token["token_type"], token["access_token"])
+        return { "Authorization": " ".join(credentials) }
+    else:
+        return False
 
 def track_item(label="BD0010915392"):
     url = "https://api.myfastway.com.au/api/track/label/"
@@ -62,7 +77,11 @@ def track_items(labels=("BD0010915392")):
     return results
 
 def main():
+    response = track_item()
+    
+    print(dumps(response, indent=4, sort_keys=True))
+
     input("Press [ENTER] to exit:")  
-    clear_screen()
+    system(CLEAR)
 
 main()
